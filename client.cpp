626296a5ee30db03b30e51837e863c1196baf223
tetsu from arch <platon160106@gmail.com>
2025-09-06 23:55:55 +0300

    a bit of changes

--- client.cpp
+++ client.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <string>
+#include <assert.h>
+
+#define SERVER_ADDRESS "127.0.0.1"
+#define SERVER_PORT 4040
 
 int main() {
     int client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    assert(client_sock > 0);
 
     sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
 
-    connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    int connection = connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    assert(connection >= 0);
+
+    std::cout << "Connected to " << SERVER_ADDRESS;
+    std::cout << ":" << SERVER_PORT << std::endl;
 
     std::string message = "Hello, server!";
     send(client_sock, message.c_str(), message.size(), 0);
 
     close(client_sock);
+    std::cout << "Client is closed" << std::endl;
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
+#include <assert.h>
+
+#define SERVER_ADDRESS "127.0.0.1"
+#define SERVER_PORT 4040
 
 int main() {
     int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    assert(server_sock > 0); 
 
     sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
 
     bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-
     listen(server_sock, 5);
+    
+    std::cout << "Server is open: " << SERVER_ADDRESS;
+    std::cout << ":" << SERVER_PORT << std::endl;
 
-    int client_sock = accept(server_sock, nullptr, nullptr);
+    size_t buf_size = 1024;
+    char buffer[buf_size];
+    int bytes_read;
+    while (true) {
+        int client_sock = accept(server_sock, nullptr, nullptr);
+        memset(buffer, 0x00, buf_size);
+        bytes_read = recv(client_sock, buffer, sizeof(buffer), 0);
 
-    char buffer[1024] = {0};
-    recv(client_sock, buffer, sizeof(buffer), 0);
-    std::cout << "Message from client: " << buffer << std::endl;
+        if (bytes_read == 0) {
+            std::cerr << "Connection is closed!\n";
+        } else if (bytes_read < 0) {
+            std::cerr << "Error with data receiving\n";
+        }
+        std::cout << "Message from client: " << buffer << std::endl;
+    }
 
     close(server_sock);
+    std::cout << "Server is closed" << std::endl;
     return 0;
 }
